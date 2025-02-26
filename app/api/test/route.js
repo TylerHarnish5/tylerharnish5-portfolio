1cf9329d832b610a824f919ee7f5d524fe7ecb84
ecarminio <carminioe@gmail.com>
2025-02-26 10:15:55 -0500

    initial commit

--- app/api/test/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({ key: process.env.OPENAI_API_KEY || "Not Found" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
